f6c4dd075502880e1cbe6ddf53b7ec6ec660f668
grant <grantmossdev@gmail.com>
2022-03-05 12:27:48 -0700

    code makes sense now

---
+++ graph_data.py
@@ -0,0 +1,183 @@
+import requests
+import pandas as pd
+import os
+import json
+import networkx as nx
+from math import radians, cos, sin, asin, sqrt
+from itertools import combinations
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
+class Data:
+    # TODO: use setter on region depending on start and end values.
+    """
+    NREL API documentation: https://developer.nrel.gov/docs/transportation/alt-fuel-stations-v1/
+    """
+
+    country_options = ['CA','US']
+
+    def __init__(self,
+                 vehicle_fuel,
+                 max_range = 500,
+                 min_range = 50,
+                 nrel_data='fuel_stations.csv',
+                 region=None):
+
+        self.vehicle_fuel = vehicle_fuel
+        self.nrel_data = nrel_data
+        self.max_range = max_range
+        self.min_range = min_range
+        self.region = region
+        self.stations = self.get_stations()
+
+
+    @staticmethod
+    def get_config_file(config_file):
+        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname("__file__")))
+        try:
+            with open(os.path.join(__location__, config_file)) as f:
+                config = json.load(f)
+                return config
+
+        except:
+            raise
+
+
+    @staticmethod
+    def api_url(key,country,url="https://developer.nrel.gov/api/alt-fuel-stations/v1.json?country=CO&api_key=YOUR_KEY_HERE",):
+        url = url.replace("YOUR_KEY_HERE", key)
+        url = url.replace("CO", country)
+        return url
+
+
+    @staticmethod
+    def request_api(url):
+        r = requests.get(url, allow_redirects=True, stream=True, headers=headers).json()  # returns a dictionary
+        df = pd.DataFrame(r["fuel_stations"])
+        return df
+
+
+    @staticmethod
+    def create_pickes(max_range, min_range):
+        fuel_options = ["ELEC", "LPG"] # , "CNG"]  # TODO: add fuel_options to self
+        # Data.country_options.append("NA")
+        for fuel in fuel_options:
+            for country in Data.country_options:
+                network = Data(vehicle_fuel=fuel, region=country)
+                network.create_graph()
+
+
+    def file_name(self):
+        return('nx_pickles/'+self.vehicle_fuel+'_'+self.region+'_'+'Max_'+str(self.max_range)+'_'+'Min_'+str(self.min_range)+'.pickle')
+
+
+    def get_stations(self):
+        if os.path.isfile(self.nrel_data):
+            used_cols = ["city", "zip", "country", "ev_pricing", "facility_type", "fuel_type_code", "latitude", "longitude", "state", "station_name", "street_address"]
+            stations = pd.read_csv(self.nrel_data, low_memory=False)
+            for col in stations:
+                if col not in used_cols:
+                    del stations[col]
+
+        else:
+            key = Data.get_config_file("api_key.json")["key"]
+            # get both countries
+            country_frames = []
+            for country in self.country_options:
+                url = Data.api_url(key, country)
+                print("api request: " + str(url))
+                df = Data.request_api(url)
+                country_frames.append(df)
+
+            stations = pd.concat(country_frames, axis=0, sort=False, ignore_index=True)
+            stations.to_csv(self.nrel_data, index=False)
+
+        stations = stations[stations["fuel_type_code"] == self.vehicle_fuel]
+        if self.region != None:
+            if self.region != 'NA':
+                stations = stations[stations["country"] == self.region].copy()
+        return stations
+
+
+
+    # from https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
+    def haversine(self, lon1, lat1, lon2, lat2):
+        """
+        Calculate the great circle distance between two points
+        on the earth (specified in decimal degrees)
+        This is set to km for default
+        """
+        # convert decimal degrees to radians
+        lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+        # haversine formula
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+        c = 2 * asin(sqrt(a))
+        r = 6371 # Radius of earth in kilometers. Use 3956 for miles, 6371 for km
+        return (int(round(c * r,0)))
+
+
+    def create_graph(self):
+        print('called create_graph')
+        '''
+        creates a semi-complete graph if there isnt already one available in binary form. The graph connectivity is limited by the maximum range.
+        A higher max range allows you to view hypothetical vehicle routes that are not available with current EV technology, but it makes the
+        optimal path calculation much slower.
+        '''
+        # TODO: look at casting types for node attributes and edge weight. This may reduce pickle file size
+        # TODO: remove limit instance variable after testing is complete!
+
+        file_name = self.file_name()
+
+        if os.path.isfile(file_name):
+            G = nx.read_gpickle(file_name)
+            print("read pickle object: " + file_name)
+        else:
+            # ceates a complete graph if there isnt one already
+            print("creating pickle object: " + file_name)
+            print("DF length: " + str(len(self.stations)))
+            G = nx.Graph()
+            for index, row in self.stations.iterrows():
+
+                # TODO: add more descriptors (columns) to the graph if neccecary
+                G.add_node(
+                    str(row["city"]) + "_" + str(row["zip"]),
+                    city=row["city"],
+                    country=row["country"],
+                    ev_pricing=row["ev_pricing"],
+                    facility_type=row["facility_type"],
+                    fuel=row["fuel_type_code"],
+                    lat=row["latitude"],
+                    long=row["longitude"],
+                    province=row["state"],
+                    station_name=row["station_name"],
+                    address=row["street_address"],
+                )
+
+            # add the graph edges
+            print("Nodes: " + str(len(G.nodes)))
+            edges = list(combinations(list(G.nodes), 2))
+            print("Edges: " + str(len(edges)))
+
+            for edge in edges:
+                lat1, long1 = G.nodes[edge[0]]["lat"], G.nodes[edge[0]]["long"]
+                lat2, long2 = G.nodes[edge[1]]["lat"], G.nodes[edge[1]]["long"]
+                distance = self.haversine(long1, lat1, long2, lat2)
+                # there is no range requirement
+
+                if distance > self.max_range or distance < self.min_range:
+                    None  # the vehicle cant make it from node 1 to node 2
+                else:
+                    G.add_edge(edge[0], edge[1], weight=distance)
+
+            # pickle the graph once it is created
+            nx.write_gpickle(G, file_name)
+            print("created new pickle object: "+ file_name+ " with max range "+ str(self.max_range))
+        return G
+
+
+if __name__ == "__main__":
+    None
+
--- nrel.py
+++ nrel.py
@@ -1,79 +1,31 @@
-import requests
-import pandas as pd
-import os
-import json
 import networkx as nx
 import random
-from math import radians, cos, sin, asin, sqrt
-from itertools import combinations
 import warnings
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+from graph_data import Data
 
 
-class Location:
+class VehicleNetwork(Data):
+    '''
+    Fuel Types: ELEC - Electric vehicle, LPG - Propane vehicle, BD - Biodiesel, CNG - compresses natural gas
+    this creates a comlete graph, meaning that all vehicle routes are possible. This network will be pruned when the
+    user enters in a range for their vehicle. I think this may be faster, especially if each Vehicle_Network is saved and imported
+    when needed
+    '''
 
-    # TODO: move the api/data methods here
-    def __init__(self, vehicle_fuel, start, end, nrel_data="fuel_stations.csv", region=None):
-        self.vehicle_fuel = vehicle_fuel
-        self.nrel_data = nrel_data
+    def __init__(self, vehicle_fuel, start, end, vehicle_range, region):
+        Data.__init__(self, vehicle_fuel=vehicle_fuel, region=region)
+        self.vehicle_fuel = vehicle_fuel #user must select one fuel type.
         self.start = start
         self.end = end
-        self.region = region #use this to import a custom region. Options are CA,US,NA
-    
-    @staticmethod
-    def config_file(config_file):
-        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname("__file__")))
+        self.vehicle_range = vehicle_range #user sets the range for their vehicle
+        self.G = self.create_graph() #read in the graph
+        self.refill_locations = self.stations#read in the df. Make sure that refill_locations is used throughout this class!
+        print('Region: '+self.region)
 
-        try:
-            with open(os.path.join(__location__, config_file)) as f:
-                config = json.load(f)
-                return config
-
-        except:
-            raise
-
-    @staticmethod
-    def api_url(key, country, url="https://developer.nrel.gov/api/alt-fuel-stations/v1.json?country=CO&api_key=YOUR_KEY_HERE"):
-        url = url.replace("YOUR_KEY_HERE", key)
-        url = url.replace("CO", country)
-        return url
-
-    @staticmethod
-    def request_api(url):
-        r = requests.get(url, allow_redirects=True, stream=True, headers=headers).json()  # returns a dictionary
-        df = pd.DataFrame(r["fuel_stations"])
-        return df
-
-    def get_stations(self):
-        if os.path.isfile(self.nrel_data):
-            used_cols = ["city", "zip", "country", "ev_pricing", "facility_type", "fuel_type_code", "latitude", "longitude", "state", "station_name", "street_address"]
-            stations = pd.read_csv(self.nrel_data, low_memory=False)
-            for col in stations:
-                if col not in used_cols:
-                    del stations[col]
-
-        else:
-            key = Data.config_file("api_key.json")["key"]
-            # get both countries
-            country_frames = []
-            for country in self.country_options:
-                url = Location.api_url(key, country)
-                print("api request: " + str(url))
-                df = Location.request_api(url)
-                country_frames.append(df)
-
-            stations = pd.concat(country_frames, axis=0, sort=False, ignore_index=True)
-            stations.to_csv(self.nrel_data, index=False)
-
-        stations = stations[stations["fuel_type_code"] == self.vehicle_fuel]
-        if self.region != None:
-            if self.region != 'NA':
-                stations = stations[stations["country"] == self.region].copy()
-        return(stations)
 
     def find_region(self):
-        df = self.get_stations()
-        
+        df = self.stations
+
         def get_random_location(loc, df):
             df = df[df["city"] == loc].copy()
             unique = [str(c).capitalize() + "_" + str(p) for c, p in zip(df["city"], df["zip"])]
@@ -86,6 +38,7 @@ class Location:
             # location = locations.sample(n=1) #should be one row of a dataframe
             n = random.choice(unique)
             return n
+
 
         def split_location(loc):
             loc = loc.lower()
@@ -121,8 +74,8 @@ class Location:
                 station_count[number_of_stations] = r
 
             return (station_count[max_stations], node)
-        
-        
+
+
         if self.region != None:
             start_country, start_node = split_location(self.start)
             end_country, end_node = split_location(self.end)
@@ -133,204 +86,18 @@ class Location:
         else:
             r = self.region
             start_node, end_node = None, None
-            
+
         if r != "NA":
             df = df[df["country"] == r].copy()
-        
-        return r, start_node, end_node, df
 
+        return start_node, end_node
 
-class Data(Location):
-    # TODO: use setter on region depending on start and end values.
-    """
-    NREL API documentation: https://developer.nrel.gov/docs/transportation/alt-fuel-stations-v1/
-    """
-    
-    country_options = ['CA','US']
-    
-    def __init__(self,
-                 vehicle_fuel,
-                 max_range = 500,
-                 min_range = 50,
-                 start=None,
-                 end=None,
-                 nrel_data='fuel_stations.csv',
-                 custom=None):
-        
-        Location.__init__(self, vehicle_fuel, start, end, region=custom)
-        self.vehicle_fuel = vehicle_fuel
-        self.nrel_data = nrel_data
-        self.max_range = max_range
-        self.min_range = min_range
-        self.start = start
-        self.end = end
-        self.region, self.start_node, self.end_node, self.stations = Location.find_region(self)
-
-    def FileName(self):
-        return('nx_pickles/'+self.vehicle_fuel+'_'+self.region+'_'+'Max_'+str(self.max_range)+'_'+'Min_'+str(self.min_range)+'.pickle')
-        
-
-    @staticmethod
-    def config_file(config_file):
-        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname("__file__")))
-
-        try:
-            with open(os.path.join(__location__, config_file)) as f:
-                config = json.load(f)
-                return config
-
-        except:
-            raise
-
-    @staticmethod
-    def api_url(key,country,url="https://developer.nrel.gov/api/alt-fuel-stations/v1.json?country=CO&api_key=YOUR_KEY_HERE",):
-        url = url.replace("YOUR_KEY_HERE", key)
-        url = url.replace("CO", country)
-        return url
-
-    @staticmethod
-    def request_api(url):
-        r = requests.get(url, allow_redirects=True, stream=True, headers=headers).json()  # returns a dictionary
-        df = pd.DataFrame(r["fuel_stations"])
-        return df
-
-    @staticmethod
-    def delete_file(name):
-        if os.path.isfile(name):
-            os.remove(name)
-
-    # from https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
-    def haversine(self, lon1, lat1, lon2, lat2):
-        """
-        Calculate the great circle distance between two points 
-        on the earth (specified in decimal degrees)
-        This is set to km for default
-        """
-        # convert decimal degrees to radians
-        lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    
-        # haversine formula 
-        dlon = lon2 - lon1 
-        dlat = lat2 - lat1 
-        a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-        c = 2 * asin(sqrt(a)) 
-        r = 6371 # Radius of earth in kilometers. Use 3956 for miles, 6371 for km
-        return (int(round(c * r,0)))
-
-    
-    def create_graph(self):
-        print('called create_graph')
-        '''
-        creates a semi-complete graph if there isnt already one available in binary form. The graph connectivity is limited by the maximum range.
-        A higher max range allows you to view hypothetical vehicle routes that are not available with current EV technology, but it makes the 
-        optimal path calculation much slower.
-        '''
-        # TODO: look at casting types for node attributes and edge weight. This may reduce pickle file size
-        # TODO: remove limit instance variable after testing is complete!
-
-        file_name = self.FileName()
-
-        if os.path.isfile(file_name):
-            G = nx.read_gpickle(file_name)
-            print("read pickle object: " + file_name)
-        else:
-            # ceates a complete graph if there isnt one already
-            print("creating pickle object: " + file_name)
-            print("DF length: " + str(len(self.stations)))
-            G = nx.Graph()
-            for index, row in self.stations.iterrows():
-
-                # TODO: add more descriptors (columns) to the graph if neccecary
-                G.add_node(
-                    str(row["city"]) + "_" + str(row["zip"]),
-                    city=row["city"],
-                    country=row["country"],
-                    ev_pricing=row["ev_pricing"],
-                    facility_type=row["facility_type"],
-                    fuel=row["fuel_type_code"],
-                    lat=row["latitude"],
-                    long=row["longitude"],
-                    province=row["state"],
-                    station_name=row["station_name"],
-                    address=row["street_address"],
-                )
-
-            # add the graph edges
-            print("Nodes: " + str(len(G.nodes)))
-            edges = list(combinations(list(G.nodes), 2))
-            print("Edges: " + str(len(edges)))
-
-            for edge in edges:
-                lat1, long1 = G.nodes[edge[0]]["lat"], G.nodes[edge[0]]["long"]
-                lat2, long2 = G.nodes[edge[1]]["lat"], G.nodes[edge[1]]["long"]
-                distance = self.haversine(long1, lat1, long2, lat2)
-                # there is no range requirement
-
-                if distance > self.max_range or distance < self.min_range:
-                    None  # the vehicle cant make it from node 1 to node 2
-                else:
-                    G.add_edge(edge[0], edge[1], weight=distance)
-
-            # pickle the graph once it is created
-            nx.write_gpickle(G, file_name)
-            print("created new pickle object: "+ file_name+ " with max range "+ str(self.max_range))
-
-        return G
-
-    @staticmethod
-    def create_pickes(max_range=None, min_range=None): #TODO: make this work...
-        '''
-        convenience method for creating all pickles at once
-        '''
-        Data.max_range = max_range
-        Data.min_range = min_range
-
-        fuel_options = ["ELEC", "LPG"] # , "CNG"]  # TODO: add fuel_options to self
-        # Data.country_options.append("NA")
-
-        for fuel in fuel_options:
-            for country in Data.country_options:
-                network = Data(start='Calgary,ab', end='Edmonton,ab', vehicle_fuel=fuel, custom=country)
-                network.create_graph()
-
-    
-            
-#TODO: make the graph (G) an intance variable. This will make it easier to modify the graph range in a VehicleNetwork method
-class VehicleNetwork(Data):
-    # TODO: NA doesnt work as a region...
-    '''
-    Fuel Types: ELEC - Electric vehicle, LPG - Propane vehicle, BD - Biodiesel, CNG - compresses natural gas 
-    this creates a comlete graph, meaning that all vehicle routes are possible. This network will be pruned when the 
-    user enters in a range for their vehicle. I think this may be faster, especially if each Vehicle_Network is saved and imported
-    when needed
-    '''
-    
-    def __init__(self, vehicle_fuel, start, end, vehicle_range, region):
-        Data.__init__(self, vehicle_fuel=vehicle_fuel, start=start, end=end, custom=region)
-        self.vehicle_fuel = vehicle_fuel #user must select one fuel type.
-        self.vehicle_range = vehicle_range #user sets the range for their vehicle
-        self.G = self.create_graph() #read in the graph
-        self.refill_locations = self.stations#read in the df. Make sure that refill_locations is used throughout this class!
-        print('Region: '+self.region)        
-    
-    
-    def Name(self):
-        return(self.vehicle_fuel+'_'+self.start+'_'+self.end+'_'+str(self.vehicle_range))
-    
-    #getter
-    def get_G(self):
-        return self.G
-
-    def source_destination(self):
-        # TODO: move the source/destination checking to the first step! This will make it faster...
-        return None
 
     # this probably isnt needed. All start/end user input should be verified in the Data class
     def available_cities(self):
-        cities = list(self.refill_locations["city"].unique())
-        return cities
+        return list(self.refill_locations["city"].unique())
 
-    # setter
+
     def vehicle_route(self):
         remove_list = []
         for paths in self.G.edges(data=True):
@@ -342,11 +109,11 @@ class VehicleNetwork(Data):
                 remove_list.append(tuple((n1, n2)))
 
         self.G.remove_edges_from(remove_list)
-        # return(self.G)
+
 
     def shortest_path(self):
 
-        source, target = self.start_node, self.end_node
+        source, target = self.find_region()
         self.vehicle_route()
 
         # TODO: make sure that self.G has been properly filtered!
@@ -406,13 +173,13 @@ class VehicleNetwork(Data):
 
 
 if __name__ == "__main__":
-    
+
     # stations = Data(vehicle_fuel='ELEC',min_range=200,max_range=400,custom='CA')
     # G = stations.create_graph()
     # df = stations.get_stations()
-    
-    Data.create_pickes(max_range=500,min_range=50)
-    # path = VehicleNetwork(vehicle_fuel='ELEC', start='Calgary,ab', end='London,on', vehicle_range=500, region="CA")
-    # route = path.shortest_path()
-    
+
+    # Data.create_pickes(max_range=500, min_range=50)
+    path = VehicleNetwork(vehicle_fuel='ELEC', start='Calgary,ab', end='London,on', vehicle_range=500, region="CA")
+    route = path.shortest_path()
+
 
