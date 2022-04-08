7cfd0d4e3816b62d5aca14bc6fba8ff12ca8b01d
grant <grantmossdev@gmail.com>
2022-04-08 11:04:41 -0600

    completed basic redis solution

--- vehicle_network_api/api.py
+++ vehicle_network_api/api.py
@@ -1,64 +1,66 @@
-from flask import Flask, session
-from flask_session import Session
+from flask import Flask
+import pickle
 import redis
 import json
 from flask_cors import CORS
 from waitress import serve
 import psutil
 import os
-import sys
 from util import set_cwd_to_script
 from vehicle_network import VehicleNetwork
 
 set_cwd_to_script()
 app = Flask(__name__)
-app.config['SESSION_TYPE'] = 'redis'
-app.config['SESSION_REDIS'] = redis.from_url('redis://localhost:6379')
-app.config['SESSION_PERMANENT'] = False
-sess = Session()
+r = redis.Redis(host='localhost', port=6379, db=0)
 CORS(app,
      origins=["http://localhost:8080"],
      expose_headers=['Access-Control-Allow-Origin'],
      supports_credentials=True)
-app.secret_key = 'supersecretkey'
-sess.init_app(app)
 process = psutil.Process(os.getpid())
 
 
 @app.route("/api/setInitialRoute", methods=["GET"])
 def set_initial_route():
-    if not "initpath" in session:
-        session["initpath"] = VehicleNetwork(
+    path = pickle.loads(r.get("initpath"))
+    if not path:
+        path = VehicleNetwork(
             vehicle_fuel="ELEC", region="CA", vehicle_range=300)
-        print("session path set", file=sys.stdout)
+        r.set("initpath", pickle.dumps(path))
+        r.set("currentpath", pickle.dumps(path))
+        print("initial path set")
     else:
-        session["initpath"] = session.get("initpath")
-    return 'ok'
+        print("initial path already set")
+        r.set("currentpath", pickle.dumps(path))
+    return "ok"
 
 
 @app.route("/api/getRoute/<start_city>/<end_city>")
 def get_route(start_city, end_city):
-    return json.dumps(session.get("initpath").shortest_path(start_city, end_city))
+    path = pickle.loads(r.get("currentpath"))
+    return json.dumps(path.shortest_path(start_city, end_city))
 
 
 @app.route("/api/updateNetwork/<f_type>/<vehicle_range>/<region>", methods=["PUT"])
 def update_network(f_type, vehicle_range, region):
-    session["initpath"] = VehicleNetwork(
+    newpath = VehicleNetwork(
         vehicle_fuel=f_type,
         region=region,
         vehicle_range=int(vehicle_range)
     )
+    r.set("currentpath", pickle.dumps(newpath))
     return json.dumps({"new_fuel": f_type, "new_range": vehicle_range, "new_region": region})
 
 
 @app.route("/api/getCityOptions", methods=["GET"])
 def get_city_options():
-    return json.dumps(session.get("initpath").available_cities())
+    path = pickle.loads(r.get("currentpath"))
+    return json.dumps(path.available_cities())
 
 
 @app.route("/api/getVehicleRange")
 def get_vehicle_range():
-    return json.dumps(session.get("initpath").vehicle_range)
+    path = pickle.loads(r.get("currentpath"))
+    return json.dumps(path.vehicle_range)
 
 
 @app.route("/api/memory")
